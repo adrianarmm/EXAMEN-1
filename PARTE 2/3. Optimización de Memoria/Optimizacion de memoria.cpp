44c4c542984f2ae992088e396950e7460eb355e9
adrianarmm <areyemor@myuax.com>
2023-11-09 11:49:06 +0100

    13

--- PARTE 2/3. Optimización de Memoria/Optimizacion de memoria.cpp
+++ PARTE 2/3. Optimización de Memoria/Optimizacion de memoria.cpp
@@ -8,3 +8,9 @@ void intercambiar(int& a, int& b) {
 int main() {
     int num1 = 2;
     int num2 = 20;
+
+    std::cout << "Antes del intercambio: num1 = " << num1 << ", num2 = " << num2 << std::endl;
+
+    intercambiar(num1, num2);
+
+    std::cout << "Después del intercambio: num1 = " << num1 << ", num2 = " << num2 << std::endl;
