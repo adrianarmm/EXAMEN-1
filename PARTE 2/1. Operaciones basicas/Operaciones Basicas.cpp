458e470fe57a02e9979aab29a88392ff4b9b1527
adrianarmm <areyemor@myuax.com>
2023-11-09 11:44:48 +0100

    3

--- PARTE 2/1. Operaciones basicas/Operaciones Basicas.cpp
+++ PARTE 2/1. Operaciones basicas/Operaciones Basicas.cpp
@@ -5,3 +5,7 @@ int sumaEnteros(int numero1, int numero2) {
 }
 int main() {
     int num1, num2;
+    std::cout << "Ingrese el primer número: ";
+    std::cin >> num1;
+    std::cout << "Ingrese el segundo número: ";
+    std::cin >> num2;
