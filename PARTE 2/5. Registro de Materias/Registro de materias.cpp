dd0cc40d6125a325b5cde5ad8ed67452576cd2bb
adrianarmm <areyemor@myuax.com>
2023-11-09 11:57:12 +0100

    25

--- PARTE 2/5. Registro de Materias/Registro de materias.cpp
+++ PARTE 2/5. Registro de Materias/Registro de materias.cpp
@@ -10,3 +10,9 @@ public:
     void registrar_materia(const std::string& materia) {
         materias.push_back(materia);
     }
+    void mostrar_materias() {
+        std::cout << "Materias registradas:" << std::endl;
+        for (const auto& materia : materias) {
+            std::cout << materia << std::endl;
+        }
+    }
