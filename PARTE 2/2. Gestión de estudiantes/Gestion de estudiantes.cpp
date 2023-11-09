838db06b57692d51e32375f75c639458f8e8f0c2
adrianarmm <areyemor@myuax.com>
2023-11-09 11:46:51 +0100

    8

--- PARTE 2/2. Gestión de estudiantes/Gestion de estudiantes.cpp
+++ PARTE 2/2. Gestión de estudiantes/Gestion de estudiantes.cpp
@@ -7,3 +7,10 @@ private:
     string nombre;
     int edad;
     string grado;
+
+public:
+    Estudiante(string nombre, int edad, string grado) {
+        this->nombre = nombre;
+        this->edad = edad;
+        this->grado = grado;
+    }
