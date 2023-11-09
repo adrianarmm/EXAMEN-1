3bb908af20bc7272b6022fe6314f45ff684a1cad
adrianarmm <areyemor@myuax.com>
2023-11-09 12:24:21 +0100

    87

--- PARTE 2/10.Registro de Profesores/Registro de profesores.cpp
+++ PARTE 2/10.Registro de Profesores/Registro de profesores.cpp
@@ -9,3 +9,17 @@ private:
     int experiencia;
 
 public:
+    Profesor(std::string nombre, int edad, std::string materia, int experiencia) {
+        this->nombre = nombre;
+        this->edad = edad;
+        this->materia = materia;
+        this->experiencia = experiencia;
+    }
+
+    void mostrar_info_profesor() {
+        std::cout << "Nombre: " << nombre << std::endl;
+        std::cout << "Edad: " << edad << std::endl;
+        std::cout << "Materia que imparte: " << materia << std::endl;
+        std::cout << "Años de experiencia: " << experiencia << std::endl;
+    }
+};
