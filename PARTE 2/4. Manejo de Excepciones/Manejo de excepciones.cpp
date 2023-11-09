7e377d5af0839a0372dafda88cda67ce6289691e
adrianarmm <areyemor@myuax.com>
2023-11-09 11:52:55 +0100

    18

--- PARTE 2/4. Manejo de Excepciones/Manejo de excepciones.cpp
+++ PARTE 2/4. Manejo de Excepciones/Manejo de excepciones.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
-void divide(int a, int b) {
+void divide(int a, int b)
+{
     if (b == 0) {
         throw "Error: División por cero";
     }
+    int result = a / b;
+    std::cout << "El resultado de la división es: " << result << std::endl;
+}
