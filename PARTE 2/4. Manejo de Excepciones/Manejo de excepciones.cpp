ad9ba68e0c597bc3e283cd1eb4a6d651bc5001c8
adrianarmm <areyemor@myuax.com>
2023-11-09 11:53:07 +0100

    19

--- PARTE 2/4. Manejo de Excepciones/Manejo de excepciones.cpp
+++ PARTE 2/4. Manejo de Excepciones/Manejo de excepciones.cpp
@@ -8,3 +8,9 @@ void divide(int a, int b)
     int result = a / b;
     std::cout << "El resultado de la división es: " << result << std::endl;
 }
+
+int num1, num2;
+std::cout << "Ingrese el primer número: ";
+std::cin >> num1;
+std::cout << "Ingrese el segundo número: ";
+std::cin >> num2;
