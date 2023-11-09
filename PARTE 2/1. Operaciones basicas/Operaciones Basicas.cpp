9794475f71deeec6eb80c037d3459f173e682162
adrianarmm <areyemor@myuax.com>
2023-11-09 11:46:22 +0100

    6

--- PARTE 2/1. Operaciones basicas/Operaciones Basicas.cpp
+++ PARTE 2/1. Operaciones basicas/Operaciones Basicas.cpp
@@ -12,7 +12,7 @@ int main() {
 
     int resultado = sumaEnteros(num1, num2);
 
-    std::cout << "La suma de 3 " << num1 << " y 2 " << num2 << " es: 5 " << resultado << std::endl;
+    std::cout << "La suma de  " << num1 << " y  " << num2 << " es:  " << resultado << std::endl;
 
     return 0;
 }
