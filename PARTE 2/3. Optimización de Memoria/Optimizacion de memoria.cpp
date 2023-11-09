4935f2cc581ac544f951127ece4fc33dd5eddd44
adrianarmm <areyemor@myuax.com>
2023-11-09 11:49:30 +0100

    15

--- PARTE 2/3. Optimización de Memoria/Optimizacion de memoria.cpp
+++ PARTE 2/3. Optimización de Memoria/Optimizacion de memoria.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 
-void intercambiar(int& a, int& b) {
+void intercambiar(int& a, int& b)
+{
     int temp = a;
     a = b;
     b = temp;
 }
-int main() {
+int main()
+{
     int num1 = 2;
     int num2 = 20;
 
